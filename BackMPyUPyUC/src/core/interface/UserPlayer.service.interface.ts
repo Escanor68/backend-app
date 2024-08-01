9b203ea0a1e9beec9dfebae5420106de8b9eaa4f
tgrebosz <tgrebosz@wibond.co>
2024-08-01 00:15:23 -0300

    cambios

--- BackMPyUPyUC/src/core/interface/UserPlayer.service.interface.ts
+++ BackMPyUPyUC/src/core/interface/UserPlayer.service.interface.ts
@@ -1,3 +1,16 @@
 export interface UserPlayerServiceInterface {
-  getName(name: string): Promise<UserPlayerObject> | null;
+  // Método para obtener todos los usuarios
+  getAll(): Promise<Array<UserPlayerObject> | null>;
+
+  // Método para obtener un usuario por su ID
+  getId(id: number): Promise<UserPlayerObject | null>;
+
+  // Método para buscar un usuario por su nombre
+  search(name: string): Promise<UserPlayerObject | null>;
+
+  // Método para insertar datos de un nuevo usuario
+  insertData(user: UserPlayerObject): Promise<void>;
+
+  // Método para actualizar los datos de un usuario existente
+  update(id: number, newData: Partial<UsersEntity>): Promise<Boolean>;
 }
