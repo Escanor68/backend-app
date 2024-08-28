9e5545fe876f7ef88d99f4bfda8767f40c42d424
Tomas Ciprian <tomasciprian98@gmail.com>
2024-08-28 11:18:02 -0300

    routes de Field

--- BackUPyUC/src/routes/routesservice.ts
+++ BackUPyUC/src/routes/routesservice.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { userPlayerController } from '../api/controller';
+import { userFieldController } from '../api/controller';
 
 const router = Router({ mergeParams: true });
 
@@ -8,9 +9,13 @@ router.post(`/api/v1/userPlayer/create`, userPlayerController.newUserPlayer);
 router.put(`/api/v1/userPlayer/update`, userPlayerController.update);
 router.put(`/api/v1/userPlayer/inactivate`, userPlayerController.inactivate);
 router.post(`/api/v1/userPlayer/request-password-reset`, userPlayerController.sendToken);
-router.post(
-    `/api/v1/userPlayer/reset-password/:token`,
-    userPlayerController.resetPassword,
-);
+router.put(`/api/v1/userPlayer/reset-password/:token`, userPlayerController.resetPassword);
+
+router.post(`/api/v1/userField/login`, userFieldController.login);
+router.post(`/api/v1/userField/create`, userFieldController.newUserField);
+router.put(`/api/v1/userField/update`, userFieldController.update);
+router.put(`/api/v1/userField/inactivate`, userFieldController.inactivate);
+router.post(`/api/v1/userField/request-password-reset`, userFieldController.sendToken);
+router.put(`/api/v1/userField/reset-password/:token`, userFieldController.resetPassword);
 
 export default router;
