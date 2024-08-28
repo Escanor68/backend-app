f2fdce101be80a5221f446844cb7b33d03eb49be
Escanor68 <abariciartg@gmail.com>
2024-08-28 10:34:12 -0300

    Definicion de las rutas para user player

--- BackUPyUC/src/routes/routesservice.ts
+++ BackUPyUC/src/routes/routesservice.ts
@@ -3,13 +3,13 @@ import { userPlayerController } from '../api/controller';
 
 const router = Router({ mergeParams: true });
 
-router.post(`/api/v1/login`, userPlayerController.login);
-router.post(`/api/v1/create`, userPlayerController.newUserPlayer);
-router.put(`/api/v1/update`, userPlayerController.update);
-router.put(`/api/v1/inactivate`, userPlayerController.inactivate);
-router.post(`/api/v1/request-password-reset`, userPlayerController.sendToken);
+router.post(`/api/v1/userPlayer/login`, userPlayerController.login);
+router.post(`/api/v1/userPlayer/create`, userPlayerController.newUserPlayer);
+router.put(`/api/v1/userPlayer/update`, userPlayerController.update);
+router.put(`/api/v1/userPlayer/inactivate`, userPlayerController.inactivate);
+router.post(`/api/v1/userPlayer/request-password-reset`, userPlayerController.sendToken);
 router.post(
-    `/api/v1/reset-password/:token`,
+    `/api/v1/userPlayer/reset-password/:token`,
     userPlayerController.resetPassword,
 );
 
